47a5120850afed17945048943463d0943732751b
Soy Rodrigo <rodri153060@gmail.com>
2024-06-12 14:14:05 -0500

    Change wallpaper

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/wallpaper_bigdata.jpg";
+import image from "../images/wallpaper3.jpg";
 
 const imageAltText = "Rodrigo Bernardo Condori Gutierrez - Future Data Engineer. This is a background photo of big data";
 
